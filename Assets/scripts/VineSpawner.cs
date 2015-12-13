5022bd2726707083358c1825e156efb3b1ae107f
Elijah <Elijahmon@live.com>
2015-12-13 01:53:41 -0500

    Elijah12/13

---
+++ Assets/scripts/VineSpawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VineSpawner : MonoBehaviour {
+
+	[SerializeField]
+	GameObject growingVines;
+
+	// Use this for initialization
+	void Start () {
+		GameObject g = (GameObject)Instantiate(growingVines, transform.position, Quaternion.identity);
+
+	}
+	
+
+}
